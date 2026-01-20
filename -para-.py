be521d8bc824de5307f6fca69c91fec66abea808
Jheniffer Melissa da Cruz Silva <mee.cruz32@gmail.com>
2026-01-20 01:27:29 -0300

    Implement simpleArraySum function and fix output
    Added a simple array sum function and adjusted print statement.

--- -para-.py
+++ -para-.py
@@ -8,3 +8,23 @@ for i in range(0, N):
     soma = soma + x
 
     print ("Soma = ", soma)
+
+#Soma simples de matriz
+def simpleArraySum(ar):
+    sum= 0
+    for i in ar:
+        sum += i
+    return sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
